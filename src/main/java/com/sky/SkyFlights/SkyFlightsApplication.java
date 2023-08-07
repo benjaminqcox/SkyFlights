a3cc78852662cdac1f436fb3adccc8f793491ecc
jdunhill <jtjdunhill@googlemail.com>
2023-08-07 16:49:07 +0100

    init commit

---
+++ src/main/java/com/sky/SkyFlights/SkyFlightsApplication.java
@@ -0,0 +1,13 @@
+package com.sky.SkyFlights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyFlightsApplication.class, args);
+	}
+
+}
