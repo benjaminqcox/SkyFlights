d8198fc78cccf14af6e9951f67410517df3ab0c2
jdunhill <jtjdunhill@googlemail.com>
2023-08-09 13:32:43 +0100

    Added complete response object

--- src/main/java/com/sky/SkyFlights/rest/BookingController.java
+++ src/main/java/com/sky/SkyFlights/rest/BookingController.java
@@ -3,14 +3,10 @@ package com.sky.SkyFlights.rest;
 
 //import com.sky.SkyFlights.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Block;
-import org.springframework.http.*;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import com.sky.SkyFlights.domain.apiResponseDomain.*;
 @RestController
@@ -39,13 +35,5 @@ public class BookingController {
                 .retrieve()
                 .bodyToMono(LocationResponse.class)
                 .block();
-
-
-//        String uri = "https://api.tequila.kiwi.com/locations/query?term=" + term +"&locale=en-US&location_types=airport&limit=2&active_only=true";
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("apikey", "9ptw_en0a60KfjnlnslcQcSRz6QjkbQ3");
-//        RestTemplate restTemplate = new RestTemplate();
-//        HttpEntity<Void> RequestEntity = new HttpEntity<>(headers);
-//        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, RequestEntity, String.class);
     }
 }
