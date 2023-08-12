b4a01d791130a0bf0f835a49289d01c7f9d3cd70
jdunhill <jtjdunhill@googlemail.com>
2023-08-12 16:17:13 +0100

    cleaned up controller URI by adding locationSearchURIBuilder class

---
+++ src/main/java/com/sky/SkyFlights/domain/apiResponseDomain/locationSearchURIBuilder.java
@@ -0,0 +1,16 @@
+package com.sky.SkyFlights.domain.apiResponseDomain;
+import com.sky.SkyFlights.domain.APIQueryParams;
+
+public class locationSearchURIBuilder {
+
+    public static String uri;
+
+    public static String uriBuilder() {
+        uri = "&locale=" + APIQueryParams.getLocale() + "&location_types=" + APIQueryParams.getLocationTypes() + "&limit=" + APIQueryParams.getReturnLimits() + "&active_only=" + APIQueryParams.getActiveOnly();
+        return uri;
+    }
+
+
+
+
+}
