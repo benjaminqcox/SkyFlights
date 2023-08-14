520ed05639cfc4ac2c0215602403b34d41396ab0
jdunhill <jtjdunhill@googlemail.com>
2023-08-14 11:03:09 +0100

    Controller now returns DTO list for more efficient data transfer to front end

--- src/main/java/com/sky/SkyFlights/dtos/FlightDTO.java
+++ src/main/java/com/sky/SkyFlights/dtos/FlightDTO.java
@@ -16,15 +16,12 @@ public class FlightDTO {
 
      String flyTo;
 
-     String duration;
+     Duration duration;
 
-     String fare;
+     Fare fare;
 
      List<String> airlines;
 
-     String departureTime;
-
-     String arrivalTime;
 
 
 
@@ -32,7 +29,7 @@ public class FlightDTO {
     }
 
 
-    public FlightDTO(String local_departure, String local_arrival, String flyFrom, String flyTo, String duration, String fare, List<String> airlines, String departureTime, String arrivalTime) {
+    public FlightDTO(String local_departure, String local_arrival, String flyFrom, String flyTo, Duration duration, Fare fare, List<String> airlines, String departureTime, String arrivalTime) {
         this.local_departure = local_departure;
         this.local_arrival = local_arrival;
         this.flyFrom = flyFrom;
@@ -40,19 +37,9 @@ public class FlightDTO {
         this.duration = duration;
         this.fare = fare;
         this.airlines = airlines;
-        this.departureTime = departureTime;
-        this.arrivalTime = arrivalTime;
+
     }
 
-    public FlightDTO(String local_departure, String local_arrival, String flyFrom, String flyTo, String duration, String fare, List<String> airlines) {
-        this.local_departure = local_departure;
-        this.local_arrival = local_arrival;
-        this.flyFrom = flyFrom;
-        this.flyTo = flyTo;
-        this.duration = duration;
-        this.fare = fare;
-        this.airlines = airlines;
-    }
 
     public String getLocal_departure() {
         return local_departure;
@@ -86,19 +73,19 @@ public class FlightDTO {
         this.flyTo = flyTo;
     }
 
-    public String getDuration() {
+    public Duration getDuration() {
         return duration;
     }
 
-    public void setDuration(String duration) {
+    public void setDuration(Duration duration) {
         this.duration = duration;
     }
 
-    public String getFare() {
+    public Fare getFare() {
         return fare;
     }
 
-    public void setFare(String fare) {
+    public void setFare(Fare fare) {
         this.fare = fare;
     }
 
@@ -110,21 +97,6 @@ public class FlightDTO {
         this.airlines = airlines;
     }
 
-    public String getDepartureTime() {
-        return departureTime;
-    }
-
-    public void setDepartureTime(String departureTime) {
-        this.departureTime = departureTime;
-    }
-
-    public String getArrivalTime() {
-        return arrivalTime;
-    }
-
-    public void setArrivalTime(String arrivalTime) {
-        this.arrivalTime = arrivalTime;
-    }
 
     @Override
     public String toString() {
--- src/main/java/com/sky/SkyFlights/rest/BookingController.java
+++ src/main/java/com/sky/SkyFlights/rest/BookingController.java
@@ -4,9 +4,13 @@ package com.sky.SkyFlights.rest;
 //import com.sky.SkyFlights.services.BookingService;
 import com.sky.SkyFlights.domain.FlightSearchAPI.FlightSearchResponse;
 import com.sky.SkyFlights.domain.FlightSearchAPI.flightSearchURIBuilder;
+import com.sky.SkyFlights.dtos.FlightDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/booking")
@@ -40,8 +44,8 @@ public class BookingController {
 //    }
 
     @GetMapping("/get/")
-    public FlightSearchResponse getOneWayFlights(@RequestParam("flyFrom") String flyFrom, @RequestParam("flyTo") String flyTo, @RequestParam("leaveDateFrom") String leaveDateFrom, @RequestParam("leaveDateTo") String leaveDateTo) throws InterruptedException {
-       return localApiClient
+    public List<FlightDTO> getOneWayFlights(@RequestParam("flyFrom") String flyFrom, @RequestParam("flyTo") String flyTo, @RequestParam("leaveDateFrom") String leaveDateFrom, @RequestParam("leaveDateTo") String leaveDateTo) throws InterruptedException {
+       FlightSearchResponse response = localApiClient
                 .get()
                 .uri("/v2/search?fly_from=" + flyFrom + "&fly_to=" + flyTo + "&date_from=" + leaveDateFrom + "&date_to=" + leaveDateTo + flightSearchURIBuilder.uriBuilder())
                 .header("apikey", "9ptw_en0a60KfjnlnslcQcSRz6QjkbQ3")
@@ -49,21 +53,22 @@ public class BookingController {
                 .bodyToMono(FlightSearchResponse.class)
                 .block();
 
-//        List<FlightDTO> flightDTOs = new ArrayList<>();
-//        System.out.println(response.getData());
-//        for (int i = 0; i < response.getData().size(); i++) {
-//            FlightDTO flightDTO = new FlightDTO();
-//            flightDTO.setLocal_departure(response.getData().get(i).getLocalDeparture());
-//            flightDTO.setLocal_arrival(response.getData().get(i).getLocalArrival());
-//            flightDTO.setFlyFrom(response.getData().get(i).getFlyFrom());
-//            flightDTO.setFlyTo(response.getData().get(i).getFlyTo());
-//            flightDTO.setDuration(String.valueOf(response.getData().get(i).getDuration()));
-//            flightDTO.setFare(String.valueOf(response.getData().get(i).getFare()));
-//            flightDTO.setAirline(response.getData().get(i).getAirlines());
-//            System.out.println(flightDTO.toString());
-//            flightDTOs.add(flightDTO);
-//
-//        }
-//        return flightDTOs;
+
+        List<FlightDTO> flightDTOs = new ArrayList<>();
+
+        for (int i = 0; i < response.getData().size(); i++) {
+            FlightDTO flightDTO = new FlightDTO();
+            flightDTO.setLocal_departure(response.getData().get(i).getLocalDeparture());
+            flightDTO.setLocal_arrival(response.getData().get(i).getLocalArrival());
+            flightDTO.setFlyFrom(response.getData().get(i).getFlyFrom());
+            flightDTO.setFlyTo(response.getData().get(i).getFlyTo());
+            flightDTO.setDuration(response.getData().get(i).getDuration());
+            flightDTO.setFare(response.getData().get(i).getFare());
+            flightDTO.setAirline(response.getData().get(i).getAirlines());
+
+            flightDTOs.add(flightDTO);
+
+        }
+        return flightDTOs;
     }
 }
